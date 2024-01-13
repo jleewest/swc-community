7b8627d03b4dc9310b5ed66a45e3184f32540f52
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-13 14:44:22 -0800

    buid:remove redux files

--- client/src/features/topics/topicsActions.js
+++
@@ -1,8 +0,0 @@
-import { CREATE_TOPIC, UPDATE_TOPIC, DELETE_TOPIC } from './topicsConstants';
-
-export function addTopic(title, body) {
-  return {
-    type: CREATE_POST,
-    payload: { title, body },
-  };
-}
