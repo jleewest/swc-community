d6d470f746dafcf72332cecc9e36e2d490b81785
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-14 13:50:05 -0800

    style:login pagae

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,21 @@
 import './App.css';
 import Display from './Display';
-import {
-  SignedOut,
-  SignedIn,
-  SignInButton,
-  //SignOutButton,
-} from '@clerk/clerk-react';
+import { SignedOut, SignedIn, SignInButton } from '@clerk/clerk-react';
 
 function App() {
   return (
     <div className='App'>
       <SignedOut>
-        <SignInButton />
-        <p>LoginPage</p>
+        <div className='app-container'>
+          <h1>California</h1>
+          <h2>Writers Club</h2>
+          <h3>Community Collaboration Page</h3>
+          <SignInButton>
+            <button className='login-button primary-button'>Log In</button>
+          </SignInButton>
+        </div>
       </SignedOut>
       <SignedIn>
-        {/*<SignOutButton afterSignOutUrl='/' />*/}
         <Display />
       </SignedIn>
     </div>
