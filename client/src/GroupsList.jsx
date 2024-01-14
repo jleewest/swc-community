73e74b3b8fb3b89a2fcc390eb58b325dfadeb8f1
Jenna Westendorf <jenna.westendorf@gmail.com>
2024-01-14 15:55:38 -0800

    feat:add GroupsList component with menu bar styling

--- client/src/GroupsList.jsx
+++ client/src/GroupsList.jsx
@@ -1,5 +1,26 @@
+import './GroupsList.css';
+
 //FORMAT FOR LIST OF ALL GROUPS
 
 export default function GroupsList() {
-  return <div className='GroupsList'>💕</div>;
+  return (
+    <div className='GroupsList'>
+      {/* MENU BAR */}
+      <div className='groups-menu-bar'>
+        <div>
+          <form className='search-bar'>
+            <input type='text' placeholder='Search for a group...' />
+            <button type='submit' className='primary-button'>
+              Search
+            </button>
+          </form>
+        </div>
+        <div>
+          <button className='primary-button new-group-button'>
+            Request new group
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
